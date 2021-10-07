53d0acc135b408c18bd0cd15b8d1fab7a23726f2
choahkimm <choahk.dev@gmail.com>
2021-10-07 18:33:31 +0900

    다형성 (polymorphism) 개념2

--- javaTPC1/src/Part2/TPC23.java
+++ javaTPC1/src/Part2/TPC23.java
@@ -1,11 +1,31 @@
 package Part2;
 
+import Part2.kr.Animal;
+import Part2.kr.Cat;
+import Part2.kr.Dog;
+
 public class TPC23 {
     public static void main(String[] args) {
         // 다형성 (polymorphism)   --- 리모콘
         // 상속 관계에 있는 클래스에서 상위 클래스가 동일한 메시지로 하위 클래스들을 서로 다르게 동작시키는 객체지향 원리
         // ex) Animal클래스가 하위 클래스인 Dog, Cat에게 동일하게 먹어라(eat)고 메시지 보냈을 때 서로 다르게 동작
+        
+        // 다형성 이론의 전제조건 => 부모 클래스를 잘 사용하라
+        // [동적 바인딩]을 통해 실현된다 (실행시점에서 사용될 메서드가 결정되는 바인딩. 프로그램 속도 저하 원인)
 
+        // 다형성 활용 방법
+        // 1. 다형성 인수(데이터 이동) : 다양한 형태의 파라미터를 받을 수 있다!
+        //    ex: public static void display(부모타입)
+        Dog d= new Dog();
+        display(d);
+        Cat c= new Cat();
+        display(c);
+    }
 
+    private static void display(Animal r) { //다형성 인수
+        r.eat();
+        if(r instanceof Cat){  // instanceof는 객체 타입을 확인하는 연산자
+            ((Cat) r).night(); // downcasting
+        }
     }
 }
---
+++ javaTPC1/src/Part2/TPC24.java
@@ -0,0 +1,35 @@
+package Part2;
+
+import Part2.kr.Animal;
+import Part2.kr.Cat;
+import Part2.kr.Dog;
+
+public class TPC24 {
+    public static void main(String[] args) {
+        // 다형성 활용 방법
+
+        // 2. 다형성 배열(서로 다른 객체를 담을 수 있다.)
+        //    부모 배열을 만들면 자식 타입이 다르다 하더라도 배열에 담을 수 있다.
+        Animal[] ani = new Animal[2];
+        ani[0] = new Dog();
+        ani[1] = new Cat();
+
+        for (int i = 0; i < ani.length; i++) { //다형성 배열
+            ani[i].eat();
+            if (ani[i] instanceof Cat) {
+                ((Cat)ani[i]).night();
+            }
+        }
+        display(ani); // 별도의 메서드를 만들어서 출력할 수도 있다!
+    }
+
+    private static void display(Animal[] ani) {
+
+        for (int i = 0; i < ani.length; i++) { // 다형성 배열
+            ani[i].eat();
+            if (ani[i] instanceof Cat) {
+                ((Cat) ani[i]).night();
+            }
+        }
+    }
+}
