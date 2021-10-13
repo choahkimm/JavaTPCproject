cbee8df4c671b33048d1b0b659703c2932bf67cb
choahkimm <choahk.dev@gmail.com>
2021-10-13 23:30:56 +0900

    인터페이스

--- javaTPC1/src/Part2/TPC26.java
+++ javaTPC1/src/Part2/TPC26.java
@@ -11,7 +11,6 @@ public class TPC26 {
         // 서로 기능이 다른 클래스 - 인터페이스
         // 인터페이스는 100퍼센트 불완전한 메서드로 이루어져야 함
 
-        // RemoCon r= new RemoCon(); 이렇게 안 됨
         RemoCon r = new TV();
         r.chDown();
         r.chUp();
