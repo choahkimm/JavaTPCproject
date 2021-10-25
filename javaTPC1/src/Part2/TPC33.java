88c9b48df5e13afa5dd0e5a82453dcd4bc2089ff
choahkimm <choahk.dev@gmail.com>
2021-10-26 00:08:05 +0900

    package

---
+++ javaTPC1/src/Part2/TPC33.java
@@ -0,0 +1,16 @@
+package Part2;
+
+import Part2.kr.inflearn.MyClass;
+
+public class TPC33 {
+    public static void main(String[] args) {
+        // package (패키지)
+        // 1. 기능이 비슷한 클래스를 모아서 관리하기 쉽게 하기 위함. 꾸러미
+        // 2. package 외부에서 접근하는 것을 막을 때 사용
+        // (패키지 내부에 있는 클래스의 접근권한이 생략되면 -> default 접근권한을 가짐)
+
+
+        // 접근 방법
+        // class full name, 접근 권한을 알아야 한다.
+    }
+}
---
+++ javaTPC1/src/Part2/kr/inflearn/MyClass.java
@@ -0,0 +1,13 @@
+package Part2.kr.inflearn;
+
+public class MyClass {
+    // default 접근 권한
+
+    public int sum(int a, int b) {
+        int hap=0;
+        for(int i=0; i<b; i++){
+            hap+=i;
+        }
+        return hap;
+    }
+}
